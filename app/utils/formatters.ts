6ed44bf426c913cb69e9cded7f2332790e58037d
Will Theodore <willtheodore@comcast.net>
2021-05-07 17:45:53 -0400

    BFX: Links not working on info pages

--- app/utils/formatters.ts
+++ app/utils/formatters.ts
@@ -1,16 +1,16 @@
 import { FSComment, FSPost, Timestamp } from "./blog";
 
 export interface Search {
-	[key: string]: string;
+  [key: string]: string;
 }
 
 export type Path = string[];
 
 export function isOfType<T>(
-	varToBeChecked: any,
-	propertyToCheckFor: keyof T
+  varToBeChecked: any,
+  propertyToCheckFor: keyof T
 ): varToBeChecked is T {
-	return (varToBeChecked as T)[propertyToCheckFor] !== undefined;
+  return (varToBeChecked as T)[propertyToCheckFor] !== undefined;
 }
 
 /**
@@ -20,18 +20,18 @@ export function isOfType<T>(
  * @returns {string} a string representation of the passed timestamp
  */
 export function formatDateForDescription(date: Timestamp): string {
-	if (date) {
-		const jsDate = new Date(date.seconds * 1000);
-		const month = jsDate.getMonth() + 1;
-		const day = jsDate.getDate();
-		const year = jsDate.getFullYear().toString().slice(2);
-		const [hours, apm] = parseHours(jsDate.getHours());
-		const minutes = parseMinutes(jsDate.getMinutes());
+  if (date) {
+    const jsDate = new Date(date.seconds * 1000);
+    const month = jsDate.getMonth() + 1;
+    const day = jsDate.getDate();
+    const year = jsDate.getFullYear().toString().slice(2);
+    const [hours, apm] = parseHours(jsDate.getHours());
+    const minutes = parseMinutes(jsDate.getMinutes());
 
-		return `${month}/${day}/${year} at ${hours}:${minutes} ${apm}`;
-	} else {
-		return "MM/DD/YY at HH:MM AM";
-	}
+    return `${month}/${day}/${year} at ${hours}:${minutes} ${apm}`;
+  } else {
+    return "MM/DD/YY at HH:MM AM";
+  }
 }
 
 /**
@@ -41,11 +41,11 @@ export function formatDateForDescription(date: Timestamp): string {
  * @returns {FSPost[]} an array of formatted posts
  */
 export function formatPosts(posts: FSPost[]): FSPost[] {
-	let result = posts.slice();
-	for (const post of result) {
-		post.formattedDate = formatDateForDescription(post.datePosted);
-	}
-	return result;
+  let result = posts.slice();
+  for (const post of result) {
+    post.formattedDate = formatDateForDescription(post.datePosted);
+  }
+  return result;
 }
 
 /**
@@ -55,12 +55,12 @@ export function formatPosts(posts: FSPost[]): FSPost[] {
  * @returns {FSComment} a firestore comment object with the formattedDate property
  */
 export function formatComment(comment: FSComment): FSComment {
-	const date = comment.datePosted;
-	const formattedDate = formatDateForDescription(date);
-	return {
-		formattedDate: formattedDate,
-		...comment,
-	};
+  const date = comment.datePosted;
+  const formattedDate = formatDateForDescription(date);
+  return {
+    formattedDate: formattedDate,
+    ...comment,
+  };
 }
 
 /**
@@ -70,26 +70,26 @@ export function formatComment(comment: FSComment): FSComment {
  * @returns {Path} an array of strings representing the total path of the location
  */
 export function parsePath(location: Location): Path {
-	if (location.pathname) {
-		const path = location.pathname;
-		let stringBuild = "";
-		let tokens: string[] = [];
-		for (let i = 0; i < path.length; i++) {
-			if (path.charAt(i) == "/") {
-				if (stringBuild != "") {
-					tokens.push(stringBuild);
-				}
-				stringBuild = "";
-			} else {
-				stringBuild = stringBuild.concat(path.charAt(i));
-			}
-		}
-		if (stringBuild != "") {
-			tokens.push(stringBuild);
-		}
-		return tokens;
-	}
-	return [];
+  if (location.pathname) {
+    const path = location.pathname;
+    let stringBuild = "";
+    let tokens: string[] = [];
+    for (let i = 0; i < path.length; i++) {
+      if (path.charAt(i) == "/") {
+        if (stringBuild != "") {
+          tokens.push(stringBuild);
+        }
+        stringBuild = "";
+      } else {
+        stringBuild = stringBuild.concat(path.charAt(i));
+      }
+    }
+    if (stringBuild != "") {
+      tokens.push(stringBuild);
+    }
+    return tokens;
+  }
+  return [];
 }
 
 /**
@@ -99,66 +99,66 @@ export function parsePath(location: Location): Path {
  * @returns {Search} an object representing the search string of the url
  */
 export function parseSearch(location: Location): Search {
-	if (location && location.search) {
-		const search = location.search;
-		let param = "";
-		let value = "";
-		let result: { [key: string]: boolean | string } = {};
+  if (location && location.search) {
+    const search = location.search;
+    let param = "";
+    let value = "";
+    let result: { [key: string]: boolean | string } = {};
 
-		for (let i = 0; i < search.length; i++) {
-			const char = search.charAt(i);
-			const paramEmpty = param.length === 0;
-			const valueEmpty = value.length === 0;
+    for (let i = 0; i < search.length; i++) {
+      const char = search.charAt(i);
+      const paramEmpty = param.length === 0;
+      const valueEmpty = value.length === 0;
 
-			switch (char) {
-				case "?":
-					if (!paramEmpty || !valueEmpty) {
-						console.log(
-							"Returning ERROR obj from parseSearch: unexpected token '?'"
-						);
-						return { error: "ERROR" };
-					}
-					break;
-				case "=":
-					if (!paramEmpty && valueEmpty) {
-						result[param] = true;
-					} else {
-						console.log(
-							"Returning ERROR obj from parseSearch: unexpected token '='"
-						);
-						return { error: "ERROR" };
-					}
-					break;
-				case "&":
-					if (result[param] && !valueEmpty) {
-						result[param] = value;
-						param = "";
-						value = "";
-					} else {
-						console.log(
-							"Returning ERROR obj from parseSearch: unexpected token '&'"
-						);
-						return { error: "Error" };
-					}
-					break;
-				default:
-					if (result[param]) {
-						value = value.concat(char);
-					} else if (valueEmpty) {
-						param = param.concat(char);
-					} else {
-						console.log(
-							"Returning ERROR obj from parseSearch: value with no param"
-						);
-					}
-			}
-		}
-		if (result[param]) {
-			result[param] = value;
-		}
-		return result as Search;
-	}
-	return { error: "ERROR" };
+      switch (char) {
+        case "?":
+          if (!paramEmpty || !valueEmpty) {
+            console.log(
+              "Returning ERROR obj from parseSearch: unexpected token '?'"
+            );
+            return { error: "ERROR" };
+          }
+          break;
+        case "=":
+          if (!paramEmpty && valueEmpty) {
+            result[param] = true;
+          } else {
+            console.log(
+              "Returning ERROR obj from parseSearch: unexpected token '='"
+            );
+            return { error: "ERROR" };
+          }
+          break;
+        case "&":
+          if (result[param] && !valueEmpty) {
+            result[param] = value;
+            param = "";
+            value = "";
+          } else {
+            console.log(
+              "Returning ERROR obj from parseSearch: unexpected token '&'"
+            );
+            return { error: "Error" };
+          }
+          break;
+        default:
+          if (result[param]) {
+            value = value.concat(char);
+          } else if (valueEmpty) {
+            param = param.concat(char);
+          } else {
+            console.log(
+              "Returning ERROR obj from parseSearch: value with no param"
+            );
+          }
+      }
+    }
+    if (result[param]) {
+      result[param] = value;
+    }
+    return result as Search;
+  }
+  return { error: "ERROR" };
 }
 
 /**
@@ -168,11 +168,11 @@ export function parseSearch(location: Location): Search {
  * @returns {string} a string representation of the minutes (e.g.: "04" instead of "4")
  */
 function parseMinutes(minutes: number): string {
-	if (minutes < 10) {
-		return `0${minutes}`;
-	} else {
-		return minutes.toString();
-	}
+  if (minutes < 10) {
+    return `0${minutes}`;
+  } else {
+    return minutes.toString();
+  }
 }
 
 /**
@@ -182,61 +182,61 @@ function parseMinutes(minutes: number): string {
  * @returns {[number, string]} a tuple containing the formatted number of hours and a string of either "AM" or "PM"
  */
 function parseHours(hours: number): [number, "AM" | "PM"] {
-	if (hours > 12) {
-		return [hours - 12, "PM"];
-	}
-	return [hours, "AM"];
+  if (hours > 12) {
+    return [hours - 12, "PM"];
+  }
+  return [hours, "AM"];
 }
 
 export function formatTime(startTime: string, endTime: string): string {
-	const startHour = startTime.slice(0, 2);
-	const endHour = endTime.slice(0, 2);
+  const startHour = startTime.slice(0, 2);
+  const endHour = endTime.slice(0, 2);
 
-	let startFormat;
-	let endFormat;
-	let startAPM;
-	let endAPM;
-	if (Number(startHour) > 12) {
-		startFormat = `${Number(startHour) - 12}:${startTime.slice(3)}`;
-		startAPM = "PM";
-	} else if (Number(startHour) === 12) {
-		startFormat = `${startTime}`;
-		startAPM = "PM";
-	} else {
-		startFormat = `${startTime}`;
-		startAPM = "AM";
-	}
+  let startFormat;
+  let endFormat;
+  let startAPM;
+  let endAPM;
+  if (Number(startHour) > 12) {
+    startFormat = `${Number(startHour) - 12}:${startTime.slice(3)}`;
+    startAPM = "PM";
+  } else if (Number(startHour) === 12) {
+    startFormat = `${startTime}`;
+    startAPM = "PM";
+  } else {
+    startFormat = `${startTime}`;
+    startAPM = "AM";
+  }
 
-	if (Number(endHour) > 12) {
-		endFormat = `${Number(endHour) - 12}:${endTime.slice(3)}`;
-		endAPM = "PM";
-	} else if (Number(endHour) === 12) {
-		endFormat = `${endTime}`;
-		endAPM = "PM";
-	} else {
-		endFormat = `${endTime}`;
-		endAPM = "AM";
-	}
+  if (Number(endHour) > 12) {
+    endFormat = `${Number(endHour) - 12}:${endTime.slice(3)}`;
+    endAPM = "PM";
+  } else if (Number(endHour) === 12) {
+    endFormat = `${endTime}`;
+    endAPM = "PM";
+  } else {
+    endFormat = `${endTime}`;
+    endAPM = "AM";
+  }
 
-	return startAPM === endAPM
-		? `${startFormat} - ${endFormat} ${startAPM}`
-		: `${startFormat} ${startAPM} - ${endFormat} ${endAPM}`;
+  return startAPM === endAPM
+    ? `${startFormat} - ${endFormat} ${startAPM}`
+    : `${startFormat} ${startAPM} - ${endFormat} ${endAPM}`;
 }
 
 export function formatLinks(text: string): string {
-	const linkExpression = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])/gi;
-	return text.replace(linkExpression, "<a href='//$&' target='_blank'>$&</a>");
+  const linkExpression = /(https?:\/\/)?[\w\-~]+(\.[\w\-~]+)+(\/[\w\-~@:%]*)*(#[\w\-]*)?(\?[^\s]*)?/gi;
+  return text.replace(linkExpression, "<a href='$&' target='_blank'>$&</a>");
 }
 
 export function formatTabs(text: string): string {
-	const tabSeparator = "%t";
-	const textParts = text.split(tabSeparator);
-	if (textParts === undefined) {
-		return text;
-	}
-	let result = "<div class='inline-tabs'>";
-	for (const part of textParts) {
-		result = result.concat(`<p>${part}</p>`);
-	}
-	return result.concat("</div>");
+  const tabSeparator = "%t";
+  const textParts = text.split(tabSeparator);
+  if (textParts === undefined) {
+    return text;
+  }
+  let result = "<div class='inline-tabs'>";
+  for (const part of textParts) {
+    result = result.concat(`<p>${part}</p>`);
+  }
+  return result.concat("</div>");
 }
